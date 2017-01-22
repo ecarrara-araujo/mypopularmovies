ee0a932698c24910397644505a239d8a97a2407a
Eduardo Carrara <ecarrara.araujo@gmail.com>
2017-01-22 21:47:35 -0200

    Added a repository for movies information to handle different movies data sources
    Signed-off-by: Eduardo Carrara <ecarrara.araujo@gmail.com>

---
+++ app/src/test/java/br/com/ecarrara/popularmovies/movies/data/repository/PopularMoviesRepositoryTest.java
@@ -0,0 +1,48 @@
+package br.com.ecarrara.popularmovies.movies.data.repository;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.io.IOException;
+import java.util.List;
+
+import br.com.ecarrara.popularmovies.movies.data.repository.datasource.MoviesDataSource;
+import br.com.ecarrara.popularmovies.movies.domain.entity.Movie;
+import br.com.ecarrara.popularmovies.testdata.movies.PopularMoviesTestData;
+import io.reactivex.Single;
+import io.reactivex.observers.TestObserver;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PopularMoviesRepositoryTest {
+
+    private final List<Movie> expectedMovieList = PopularMoviesTestData.POPULAR_MOVIES_LIST;
+
+    @Mock
+    private MoviesDataSource mockMoviesDataSource;
+
+    private MoviesRepository moviesRepository;
+
+    @Before
+    public void setUp() throws IOException {
+        moviesRepository = new MoviesRepositoryImpl(mockMoviesDataSource);
+    }
+
+    @Test
+    public void successfulListPopularMovies() throws Exception {
+        when(mockMoviesDataSource.listPopularMovies()).thenReturn(Single.just(expectedMovieList));
+        TestObserver<List<Movie>> testObserver = new TestObserver<>();
+
+        moviesRepository
+                .listPopularMovies()
+                .subscribe(testObserver);
+
+        testObserver.assertResult(expectedMovieList);
+    }
+
+
+}
---
+++ app/src/test/java/br/com/ecarrara/popularmovies/movies/data/repository/TopRatedMoviesRepositoryTest.java
@@ -0,0 +1,49 @@
+package br.com.ecarrara.popularmovies.movies.data.repository;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.io.IOException;
+import java.util.List;
+
+import br.com.ecarrara.popularmovies.movies.data.repository.datasource.MoviesDataSource;
+import br.com.ecarrara.popularmovies.movies.domain.entity.Movie;
+import br.com.ecarrara.popularmovies.testdata.movies.PopularMoviesTestData;
+import br.com.ecarrara.popularmovies.testdata.movies.TopRatedMoviesTestData;
+import io.reactivex.Single;
+import io.reactivex.observers.TestObserver;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TopRatedMoviesRepositoryTest {
+
+    private final List<Movie> expectedMovieList = TopRatedMoviesTestData.TOP_RATED_MOVIES_LIST;
+
+    @Mock
+    private MoviesDataSource mockMoviesDataSource;
+
+    private MoviesRepository moviesRepository;
+
+    @Before
+    public void setUp() throws IOException {
+        moviesRepository = new MoviesRepositoryImpl(mockMoviesDataSource);
+    }
+
+    @Test
+    public void successfulListPopularMovies() throws Exception {
+        when(mockMoviesDataSource.listTopRatedMovies()).thenReturn(Single.just(expectedMovieList));
+        TestObserver<List<Movie>> testObserver = new TestObserver<>();
+
+        moviesRepository
+                .listTopRatedMovies()
+                .subscribe(testObserver);
+
+        testObserver.assertResult(expectedMovieList);
+    }
+
+
+}
