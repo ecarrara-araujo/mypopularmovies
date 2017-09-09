8e239cff355052fbe42f4fd07bc659ce76692473
Eduardo Carrara <ecarrara.araujo@gmail.com>
2017-09-09 15:28:22 -0300

    refactor: MovieDetailViewModel to AutoValue.Builder
    Signed-off-by: Eduardo Carrara <ecarrara.araujo@gmail.com>

--- app/src/main/java/br/com/ecarrara/popularmovies/movies/presentation/model/MovieDetailViewModel.java
+++ app/src/main/java/br/com/ecarrara/popularmovies/movies/presentation/model/MovieDetailViewModel.java
@@ -2,62 +2,38 @@ package br.com.ecarrara.popularmovies.movies.presentation.model;
 
 import com.google.auto.value.AutoValue;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import br.com.ecarrara.popularmovies.core.networking.rest.RestImageUrlBuilder;
-import br.com.ecarrara.popularmovies.movies.domain.entity.Movie;
-
-@AutoValue public abstract class MovieDetailViewModel {
+@AutoValue
+public abstract class MovieDetailViewModel {
 
     public abstract String title();
+
     public abstract String releaseDate();
+
     public abstract String posterPath();
+
     public abstract Double voteAverage();
+
     public abstract String plotSynopsis();
 
-    public static class Builder {
-        private String title = Movie.NO_TITLE;
-        private String releaseDate = Movie.INVALID_FORMATTED_DATE;
-        private String plotSynopsis = Movie.NO_OVERVIEW;
-        private Double voteAverage = Movie.NO_VOTE_AVERAGE;
-        private String posterPath = Movie.NO_POSTER_PATH;
+    public abstract Builder toBuilder();
 
-        public MovieDetailViewModel build() {
-            return new AutoValue_MovieDetailViewModel(
-                this.title,
-                this.releaseDate,
-                this.posterPath,
-                this.voteAverage,
-                this.plotSynopsis
-            );
-        }
+    public static Builder builder() {
+        return new AutoValue_MovieDetailViewModel.Builder();
+    }
 
-        public Builder(String title) {
-            this.title = title;
-        }
+    @AutoValue.Builder
+    abstract static class Builder {
+        abstract Builder setTitle(String title);
 
-        public Builder setDate(Date releaseDate) {
-            DateFormat dateFormat = SimpleDateFormat.getDateInstance();
-            this.releaseDate = dateFormat.format(releaseDate);
-            return this;
-        }
+        abstract Builder setReleaseDate(String releaseDate);
 
-        public Builder setPosterPath(String posterPath) {
-            this.posterPath = RestImageUrlBuilder.build(posterPath);
-            return this;
-        }
+        abstract Builder setPosterPath(String posterPath);
 
-        public Builder setVoteAverage(Double voteAverage) {
-            this.voteAverage = voteAverage;
-            return this;
-        }
+        abstract Builder setVoteAverage(Double voteAverage);
 
-        public Builder setPlotSynopsis(String plotSynopsis) {
-            this.plotSynopsis = plotSynopsis;
-            return this;
-        }
+        abstract Builder setPlotSynopsis(String plotSynopsis);
+
+        abstract MovieDetailViewModel build();
     }
 
 }
--- app/src/main/java/br/com/ecarrara/popularmovies/movies/presentation/model/MovieDetailViewModelMapper.java
+++ app/src/main/java/br/com/ecarrara/popularmovies/movies/presentation/model/MovieDetailViewModelMapper.java
@@ -1,12 +1,18 @@
 package br.com.ecarrara.popularmovies.movies.presentation.model;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import br.com.ecarrara.popularmovies.core.networking.rest.RestImageUrlBuilder;
 import br.com.ecarrara.popularmovies.movies.domain.entity.Movie;
 
 public class MovieDetailViewModelMapper {
     public static MovieDetailViewModel transformFrom(Movie movie) {
-        return new MovieDetailViewModel.Builder(movie.originalTitle())
-                .setDate(movie.releaseDate())
-                .setPosterPath(movie.posterPath())
+        return MovieDetailViewModel.builder()
+                .setTitle(movie.originalTitle())
+                .setReleaseDate(SimpleDateFormat.getDateInstance().format(movie.releaseDate()))
+                .setPosterPath(RestImageUrlBuilder.build(movie.posterPath()))
                 .setVoteAverage(movie.voteAverage())
                 .setPlotSynopsis(movie.overview())
                 .build();
